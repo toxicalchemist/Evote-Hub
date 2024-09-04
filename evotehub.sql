4a5ac1235ce6fef3553e43a42713270b3c371483
toxicalchemist <141489794+toxicalchemist@users.noreply.github.com>
2024-09-04 12:08:22 +0800

    Add files via upload

---
+++ evotehub.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 04, 2024 at 06:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `evotehub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidate_details`
+--
+
+CREATE TABLE `candidate_details` (
+  `id` int(11) NOT NULL,
+  `election_id` int(11) DEFAULT NULL,
+  `candidate_name` varchar(255) DEFAULT NULL,
+  `candidate_details` text NOT NULL,
+  `candidate_photo` text DEFAULT NULL,
+  `inserted_by` varchar(255) DEFAULT NULL,
+  `inserted_on` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elections`
+--
+
+CREATE TABLE `elections` (
+  `id` int(11) NOT NULL,
+  `election_topic` varchar(255) DEFAULT NULL,
+  `no_of_candidates` int(45) DEFAULT NULL,
+  `starting_date` date DEFAULT NULL,
+  `ending_date` date DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `inserted_by` varchar(255) DEFAULT NULL,
+  `inserted_on` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `elections`
+--
+
+INSERT INTO `elections` (`id`, `election_topic`, `no_of_candidates`, `starting_date`, `ending_date`, `status`, `inserted_by`, `inserted_on`) VALUES
+(2, 'President', 5, '2024-09-04', '2024-09-21', 'Active', '', '2024-09-04'),
+(3, 'Secretary', 3, '2024-09-05', '2024-09-19', 'InActive', '', '2024-09-04'),
+(4, 'bahahahaha', 2, '2024-09-04', '2024-09-26', 'Active', '', '2024-09-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `student_no` varchar(255) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `user_role` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `student_no`, `password`, `user_role`) VALUES
+(1, '', '0', 'b521caa6e1db82e5a01c924a419870cb72b81635', 'Admin'),
+(2, '', '24', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Voter'),
+(3, '', 'Admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Admin'),
+(4, '', 'voter', '9b9bd61011415da8f033951783c7391ba15c82d5', 'Voter');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votings`
+--
+
+CREATE TABLE `votings` (
+  `id` int(11) NOT NULL,
+  `election_id` int(11) DEFAULT NULL,
+  `voters_id` int(11) DEFAULT NULL,
+  `candidate_id` int(11) DEFAULT NULL,
+  `vote_date` date DEFAULT NULL,
+  `vote_time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `votings`
+--
+
+INSERT INTO `votings` (`id`, `election_id`, `voters_id`, `candidate_id`, `vote_date`, `vote_time`) VALUES
+(1, 2, 4, 1, '2024-09-04', '05:15:27'),
+(2, 4, 4, 3, '2024-09-04', '05:41:43');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `candidate_details`
+--
+ALTER TABLE `candidate_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `elections`
+--
+ALTER TABLE `elections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `votings`
+--
+ALTER TABLE `votings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `candidate_details`
+--
+ALTER TABLE `candidate_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `elections`
+--
+ALTER TABLE `elections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `votings`
+--
+ALTER TABLE `votings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
